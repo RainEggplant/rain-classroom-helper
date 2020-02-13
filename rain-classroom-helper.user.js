391dd9ab20e1501bef9ae6130f52f374a38dda96
RainEggplant <i@raineggplant.com>
2020-02-13 18:57:08 +0800

    嵌入 waitForKeyElement, 不再通过 @require 外部地址

--- rain-classroom-helper.user.js
+++ rain-classroom-helper.user.js
@@ -7,7 +7,6 @@
 // @match        *://www.yuketang.cn/web*
 // @grant        GM_addStyle
 // @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
-// @require      https://gitee.com/RainEggplant/codes/a0votqgd8rfh73yw6z21496/raw?blob_name=waitForKeyElements.js
 // @updateURL    https://raw.githubusercontent.com/RainEggplant/rain-classroom-helper/master/rain-classroom-helper.user.js
 // @downloadURL  https://raw.githubusercontent.com/RainEggplant/rain-classroom-helper/master/rain-classroom-helper.user.js
 // @homepageURL  https://github.com/RainEggplant/rain-classroom-helper
@@ -125,6 +124,9 @@
     $('ul.nav-list').append(liAbout);
   });
 
+  // 中间 iframe 加载出内容后绑定处理视频的函数
+  waitForKeyElements('body', addVideoHandler, true, '#rainiframe');
+
   // 启动将课件中视频提到右边栏播放的处理函数
   function startObservation() {
     const rainIFrame = document.getElementById('rainiframe').contentWindow
@@ -187,5 +189,87 @@
     });
   }
 
-  waitForKeyElements('body', addVideoHandler, true, '#rainiframe');
+
+  // ==== DO NOT MODIFY
+  // ==== third-party utility functions:
+  /*--- waitForKeyElements():  A utility function, for Greasemonkey scripts,
+    that detects and handles AJAXed content.
+    IMPORTANT: This function requires your script to have loaded jQuery.
+  */
+  function waitForKeyElements(
+    selectorTxt,    /* Required: The jQuery selector string that
+                      specifies the desired element(s).
+                  */
+    actionFunction, /* Required: The code to run when elements are
+                      found. It is passed a jNode to the matched
+                      element.
+                  */
+    bWaitOnce,      /* Optional: If false, will continue to scan for
+                      new elements even after the first match is
+                      found.
+                  */
+    iframeSelector  /* Optional: If set, identifies the iframe to
+                      search.
+                  */
+  ) {
+    var targetNodes, btargetsFound;
+
+    if (typeof iframeSelector == 'undefined')
+      targetNodes = $(selectorTxt);
+    else
+      targetNodes = $(iframeSelector).contents()
+        .find(selectorTxt);
+
+    if (targetNodes && targetNodes.length > 0) {
+      btargetsFound = true;
+      /*--- Found target node(s).  Go through each and act if they
+          are new.
+      */
+      targetNodes.each(function () {
+        var jThis = $(this);
+        var alreadyFound = jThis.data('alreadyFound') || false;
+
+        if (!alreadyFound) {
+          //--- Call the payload function.
+          var cancelFound = actionFunction(jThis);
+          if (cancelFound)
+            btargetsFound = false;
+          else
+            jThis.data('alreadyFound', true);
+        }
+      });
+    }
+    else {
+      btargetsFound = false;
+    }
+
+    //--- Get the timer-control variable for this selector.
+    var controlObj = waitForKeyElements.controlObj || {};
+    var controlKey = selectorTxt.replace(/[^\w]/g, '_');
+    var timeControl = controlObj[controlKey];
+
+    //--- Now set or clear the timer as appropriate.
+    if (btargetsFound && bWaitOnce && timeControl) {
+      //--- The only condition where we need to clear the timer.
+      clearInterval(timeControl);
+      delete controlObj[controlKey];
+    }
+    else {
+      //--- Set a timer, if needed.
+      if (!timeControl) {
+        timeControl = setInterval(function () {
+          waitForKeyElements(selectorTxt,
+            actionFunction,
+            bWaitOnce,
+            iframeSelector
+          );
+        },
+        300
+        );
+        controlObj[controlKey] = timeControl;
+      }
+    }
+    waitForKeyElements.controlObj = controlObj;
+  }
+
 })();
