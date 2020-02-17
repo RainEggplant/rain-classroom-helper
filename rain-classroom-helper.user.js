fac8ca99683cffb410cb3337eb1dbd446e8f5cba
xulouzhe <43928179+xulouzhe@users.noreply.github.com>
2020-02-17 20:28:01 +0800

    增加了对荷塘雨课堂的支持 (#5)
    * 增加了对荷塘雨课堂的支持
    
    * Update rain-classroom-helper.user.js
    
    Co-authored-by: RainEggplant <i@raineggplant.com>

--- rain-classroom-helper.user.js
+++ rain-classroom-helper.user.js
@@ -5,6 +5,7 @@
 // @description  优化雨课堂使用体验
 // @author       RainEggplant
 // @match        *://www.yuketang.cn/web*
+// @match        *://pro.yuketang.cn/web*
 // @grant        GM_addStyle
 // @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
 // @updateURL    https://raw.githubusercontent.com/RainEggplant/rain-classroom-helper/master/rain-classroom-helper.user.js
